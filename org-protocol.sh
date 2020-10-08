6a68d0718df04afb758c8703c414953b2d107ae4
vanangamudi <selva.developer@gmail.com>
2020-10-09 00:36:01 +0530

    emacs daemon and org-protocol

---
+++ org-protocol.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+emacsclient --create-frame \
+            --socket-name 'capture' \
+            --alternate-editor='' \
+            --frame-parameters='(quote (name . "capture"))' \
+            --no-wait \
+            --eval "(my/org-capture-frame)"
