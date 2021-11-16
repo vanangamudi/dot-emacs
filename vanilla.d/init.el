dfae597adce957adac97f4f9549cffeca89c17e0
vanangamudi <selva.developer@gmail.com>
2021-11-17 02:16:22 +0530

    prints how long it took to boot emacs

--- vanilla.d/init.el
+++ vanilla.d/init.el
@@ -26,11 +26,20 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   (quote
-    (htmlize org-plus-contrib which-key try use-package))))
+   '(vlf org-noter htmlize org-plus-contrib which-key try use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(run-with-idle-timer
+     3 nil
+     (lambda ()
+       (let ((inhibit-message t))
+         (message "Emacs ready in %s with %d garbage collections."
+                  (format "%.2f seconds"
+                          (float-time
+                           (time-subtract after-init-time before-init-time)))
+                  gcs-done))))
