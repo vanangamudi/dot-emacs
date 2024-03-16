ea760b4070323d45fa7766cbbe11dbc421ce8462
vanangamudi <selva.developer@gmail.com>
2024-03-16 21:15:26 +0530

    adi emacs for basic emacs configuration

---
+++ adi-emacs/init.el
@@ -0,0 +1,32 @@
+
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives  '("org"       . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives  '("melpa"     . "https://melpa.org/packages/"))
+(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+
+(require 'org-tempo)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell . t)
+   (python . t)))
+
+
+(run-with-idle-timer
+ 3 nil
+ (lambda ()
+   (let ((inhibit-message t))
+     (message "Emacs ready in %s with %d garbage collections."
+              (format "%.2f seconds"
+                      (float-time
+                       (time-subtract after-init-time before-init-time)))
+              gcs-done))))
