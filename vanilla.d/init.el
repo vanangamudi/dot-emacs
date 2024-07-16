b105c4ea9e0b29a29c919e0e3c16e8799ddf1fd5
vanangamudi <selva.developer@gmail.com>
2024-07-16 18:00:39 +0530

    cleanup init.el

--- vanilla.d/init.el
+++ vanilla.d/init.el
@@ -18,19 +18,6 @@
 ;; load org package and our emacs-config.org file
 (require 'org)
 (org-babel-load-file "~/emacs/vanilla.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (run-with-idle-timer
      3 nil
