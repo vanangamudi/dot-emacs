4d0375c3adeb4ad54aad8ca13bff580d4fac42e8
vanangamudi <selva.developer@gmail.com>
2025-07-09 15:11:26 +0530

    remove old elpaca setup code

--- org-protocol.sh
+++ org-protocol.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 emacsclient --create-frame \
-            --socket-name 'capture' \
-            --alternate-editor='' \
-            --frame-parameters='(quote (name . "capture"))' \
+            #--socket-name "capture" \
+            --alternate-editor="" \
+            --frame-parameters="(quote (name . \"capture\"))" \
             --no-wait \
             --eval "(my/org-capture-frame)"
